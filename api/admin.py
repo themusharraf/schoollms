c2a530b5b5abfa3c3b04f2fb80b8e0613e2acd45
mrx <meibrohimov@gmail.com>
2024-10-02 15:16:09 +0500

    add model change

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,78 @@
 from django.contrib import admin
-from api.models import Student, Lesson, Modul
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from api.models import GroupClass, GroupLessons, Science, Modul, User, Lesson, LessonSubmission
 
-admin.site.register(Student)
-admin.site.register(Lesson)
-admin.site.register(Modul)
+
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+    # Customizing list display
+    list_display = ('username', 'email', 'full_name', 'is_teacher', 'is_staff', 'is_active')
+    search_fields = ('username', 'email', 'full_name')
+    list_filter = ('is_teacher', 'is_staff', 'is_superuser', 'is_active', 'groups')
+
+    # Fields to be shown in User change form (admin page to edit user)
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('full_name', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_teacher', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    # Fields to be shown in User creation form (admin page to add a user)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'full_name', 'email', 'password1', 'password2'),
+        }),
+    )
+
+    # Defining ordering in the list display
+    ordering = ('username',)
+
+    # Fields displayed in the user detail view (after clicking on a user)
+    readonly_fields = ('last_login', 'date_joined')
+
+
+@admin.register(GroupClass)
+class GroupClassAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created', 'updated')  # Display group name and timestamps
+    search_fields = ('name',)  # Enable search by group name
+    filter_horizontal = ('students',)  # For ManyToMany fields
+
+
+@admin.register(GroupLessons)
+class GroupLessonsAdmin(admin.ModelAdmin):
+    list_display = ('group', 'science', 'teacher', 'created', 'updated')
+    search_fields = ('group__name', 'science__name', 'teacher__full_name')  # Enable search by related fields
+    list_filter = ('science', 'teacher')
+
+
+@admin.register(Science)
+class ScienceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created', 'updated')
+    search_fields = ('name',)
+
+
+@admin.register(Modul)
+class ModulAdmin(admin.ModelAdmin):
+    list_display = ('name', 'science', 'created', 'updated')
+    search_fields = ('name', 'science__name')
+    list_filter = ('science',)
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'modul', 'created', 'updated')
+    search_fields = ('name', 'modul__name')
+    list_filter = ('modul',)
+    filter_horizontal = ('students',)
+
+
+@admin.register(LessonSubmission)
+class LessonSubmissionAdmin(admin.ModelAdmin):
+    list_display = ('student', 'lesson', 'grade', 'created', 'updated')
+    search_fields = ('student__full_name', 'lesson__name')
+    list_filter = ('grade',)
--- api/models.py
+++ api/models.py
@@ -11,8 +11,15 @@ class BaseDate(models.Model):
         abstract = True
 
 
-class Class(BaseDate):
+class GroupClass(BaseDate):
     name = models.CharField(max_length=255)
+    students = models.ManyToManyField('User')
+
+
+class GroupLessons(BaseDate):
+    group = models.ForeignKey(GroupClass, related_name='lessons', on_delete=models.CASCADE)
+    science = models.ForeignKey('Science', related_name='groups', on_delete=models.PROTECT)
+    teacher = models.ForeignKey('User', related_name='groups_class', on_delete=models.PROTECT)
 
 
 class Science(BaseDate):
@@ -33,18 +40,8 @@ class User(AbstractUser):
     full_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)  # Django'ning o'zida parolni boshqarish # noqa
-    class_name = models.ManyToManyField('Modul', related_name='teacher')
-
-    def __str__(self):
-        return self.full_name
-
-
-class Student(models.Model):
-    full_name = models.CharField(max_length=255)
-    great = models.CharField(max_length=5, null=True, blank=True, default=None)  # default null
-    class_name = models.ForeignKey(Class, on_delete=models.CASCADE,
-                                   related_name='students')  # ForeignKey orqali bog'lash # noqa
-    moduls = models.ManyToManyField('Modul', related_name='students')  # Many-to-Many bog'lanish Modullar bilan # noqa
+    is_teacher = models.BooleanField(default=False)
+    status = models.BooleanField(default=True)
 
     def __str__(self):
         return self.full_name
@@ -57,7 +54,7 @@ class Lesson(BaseDate):
     name = models.CharField(max_length=255, null=True, blank=True)
     lesson_text = models.TextField()
 
-    students = models.ManyToManyField(Student, related_name='lessons')  # students list
+    students = models.ManyToManyField(User, related_name='lessons')  # students list
 
     def __str__(self):
         return f"Lesson on {self.date}"
@@ -66,9 +63,9 @@ class Lesson(BaseDate):
 class LessonSubmission(BaseDate):
     """Darslarda o'quvchilarni baholash uchun model"""  # noqa
 
-    student = models.ForeignKey(Student, related_name='grades',
+    student = models.ForeignKey(User, related_name='grades',
                                 on_delete=models.PROTECT)  # 1 ta student uchun 1 ta baho # noqa
-    grade = models.CharField(max_length=5, null=True, blank=True, default=None)
+    grade = models.CharField(max_length=5, null=True, blank=True)
     lesson = models.ForeignKey(Lesson, related_name='grades', on_delete=models.PROTECT)  # get post
 
     def __str__(self):
